ea1be4575b32f363f478abe404a8249d8b8e4a3c
Thomas Le Baron <thomas.lebaron@free.fr>
2018-03-20 17:48:31 -0400

    contributor test

--- WPInterface/src/helloWorld.java
+++ WPInterface/src/helloWorld.java
@@ -3,7 +3,8 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+		System.out.println("hello world with contributor test");
+		
 	}
 
 }
