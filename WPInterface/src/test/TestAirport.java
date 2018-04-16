03d9f48b643e9054326635cba2d1ffa04e7c5af8
kartikvasu <kartiktv97@gmail.com>
2018-04-16 16:31:47 -0400

    testing airport

---
+++ WPInterface/src/test/TestAirport.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAirport {
+
+	@Test
+	public void testADdition() {
+		String hello = "hello";
+		assertEquals(hello, "hllo");
+	}
+
+}
